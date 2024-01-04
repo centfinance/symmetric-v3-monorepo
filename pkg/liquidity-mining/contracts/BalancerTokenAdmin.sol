f0e08fab98ae392c7a3bd0960bbe693034585e60
stephen <991266+stxphxn@users.noreply.github.com>
2024-01-04 16:28:07 +0000

    Update BalancerTokenAdmin.sol

--- pkg/liquidity-mining/contracts/BalancerTokenAdmin.sol
+++ pkg/liquidity-mining/contracts/BalancerTokenAdmin.sol
@@ -38,7 +38,7 @@ contract BalancerTokenAdmin is IBalancerTokenAdmin, SingletonAuthentication, Ree
     using Math for uint256;
 
     // Initial inflation rate of 145k BAL per week.
-    uint256 public constant override INITIAL_RATE = (145000 * 1e18) / uint256(1 weeks); // BAL has 18 decimals
+    uint256 public constant override INITIAL_RATE = (109695 * 1e18) / uint256(1 weeks); // BAL has 18 decimals
     uint256 public constant override RATE_REDUCTION_TIME = 365 days;
     uint256 public constant override RATE_REDUCTION_COEFFICIENT = 1189207115002721024; // 2 ** (1/4) * 1e18
     uint256 public constant override RATE_DENOMINATOR = 1e18;
